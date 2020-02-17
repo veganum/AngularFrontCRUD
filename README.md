db0bebb9325f3b9b59f17fd96458ee6c18cd548f
José <veganum@hotmail.com>
2020-02-17 23:23:11 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# CRUD ANGULAR
+                                          
+
+
+
+Para ver el proyecto: 
+
+Proyecto para el FrontEnd de un CRUD con el Framework Angular
+
+## License & Copyright
+© Jose Franco Nieto
---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent {
+
+  listaCurso: string[] = ['TypeScript', 'JavaScript', 'Java Se', 'C#', 'PHP'];
+
+  constructor() { }
+}
