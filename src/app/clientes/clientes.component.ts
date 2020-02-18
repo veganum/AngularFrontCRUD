01147cb18729422c1e41040f618fca4e05c77ade
José <veganum@hotmail.com>
2020-02-18 10:46:56 +0100

    Creando archivo clientes.json.ts con la lista de objetos

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -7,22 +8,12 @@ import { Cliente } from './cliente';
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    { id: 1, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020-12-11' },
-    { id: 2, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
-    { id: 3, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
-    { id: 4, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
-    { id: 5, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
-    { id: 6, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
-    { id: 7, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
-    { id: 8, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
-    { id: 9, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
-    { id: 10, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' }
-  ];
+  clientes: Cliente[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.clientes = CLIENTES;
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    { id: 1, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020-12-11' },
+    { id: 2, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 3, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 4, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 5, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 6, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 7, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 8, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 9, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 10, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' }
+];
