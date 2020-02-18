c0a87d1ad8a5381a4997e3aad5a834565c711837
José <veganum@hotmail.com>
2020-02-18 10:39:50 +0100

    Listando los objetos del tipo Cliente

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,10 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
 export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[] = [
+    { id: 1, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020-12-11' },
+    { id: 2, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 3, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 4, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 5, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 6, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 7, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 8, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 9, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' },
+    { id: 10, nombre: 'José', apellido: 'Franco', email: 'mymail@gmail.com', createAt: '2020' }
+  ];
 
   constructor() { }
 
